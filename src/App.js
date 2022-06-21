62c323c40aa7b30a655487d55971dc3e17c42c8b
techFreak <devhalltech@gmail.com>
2022-06-21 11:17:55 +0100

    Initial page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <h1>Celahel</h1>
+  <h3 className="text-4xl text-primary font-bold underline">
+      Hello world!
+    </h3>
+    <p>welcome bro</p>
     </div>
   );
 }
